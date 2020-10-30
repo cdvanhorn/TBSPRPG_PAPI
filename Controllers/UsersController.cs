071f5fcd488a7f4de5ebba7ba8e9e1ab99f20981
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-29 21:15:53 -0400

    Microservices
    Authorization works, getting a list of users works.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -14,19 +14,20 @@ namespace UserApi.Controllers {
     [Route("api/[controller]")]
     public class UsersController : ControllerBase {
         private IJwtHelper _jwtHelper;
+        private RestClient _client;
 
         public UsersController(IJwtHelper jwtHelper) {
             _jwtHelper = jwtHelper;
+            _client = new RestClient("http://userapi:8001/api/");
         }
 
         [HttpPost("authenticate")]
         public async Task<IActionResult> Authenticate(AuthenticateRequest model)
         {
             //make a request to our user microservice, we're doing this synchronouse
-            var client = new RestClient("http://userapi:8001/api/");
             var request = new RestRequest("users/authenticate", DataFormat.Json);
             request.AddJsonBody(model);
-            var response = await client.ExecutePostAsync<AuthenticateResponse>(request);
+            var response = await _client.ExecutePostAsync<AuthenticateResponse>(request);
 
             if(!response.IsSuccessful)
                 return BadRequest(new { message = "Username or password is incorrect" });
@@ -36,11 +37,15 @@ namespace UserApi.Controllers {
             return Ok(response.Data);
         }
 
-        // [HttpGet]
-        // public async Task<IActionResult> GetAll()
-        // {
-        //     //var users = await _userService.GetAll();
-        //     return Ok(/*users*/);
-        // }
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetAll()
+        {
+            var request = new RestRequest("users", DataFormat.Json);
+            var response = await _client.ExecuteGetAsync(request);
+            if(!response.IsSuccessful)
+                return BadRequest( new { message = response.ErrorMessage });
+            return Ok(response.Content);
+        }
     }
 }
