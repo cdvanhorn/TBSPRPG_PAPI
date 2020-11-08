8fa10e12b4009742ee962a66a7d9ba609268cb8d
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-11-07 23:01:15 -0500

    Game Service
    Now using shared library for jwt token generation.

--- PublicApi/Utilities/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace PublicApi.Utilities {
-    public class JwtSettings : IJwtSettings{
-        public string Secret { get; set; }
-    }
-
-    public interface IJwtSettings {
-        string Secret { get; set; }
-    }
-}
