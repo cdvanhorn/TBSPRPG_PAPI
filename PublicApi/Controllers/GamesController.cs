36260739f1801a1f80ef7d2a734fc3073bec3e6b
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-03-15 21:00:52 -0400

    TbspRpgLib
    Added Game controller endpoints

--- PublicApi/Controllers/GamesController.cs
+++ PublicApi/Controllers/GamesController.cs
@@ -1,22 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 
-using RestSharp;
-
 using System;
 using System.Threading.Tasks;
+using TbspRpgLib.InterServiceCommunication;
 
 namespace PublicApi.Controllers {
     
     [ApiController]
     [Route("api/[controller]")]
     public class GamesController : BaseController {
+        private IGameServiceLink _serviceLink;
 
-        // public GamesController() : base("http://gameapi:8003/api/") { }
+        public GamesController(IGameServiceLink serviceLink) {
+            _serviceLink = serviceLink;
+        }
 
-        // [Route("start/{name}")]
-        // [Authorize]
-        // public async Task<IActionResult> Start(string name) {
-        //     return await MakeGetServiceRequest($"games/start/{name}");
-        // }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetAllForUser() {
+            return Respond(
+                await _serviceLink.CR_GetGames(CreateCredentials())
+            );
+        }
+
+        [Authorize]
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetByAdventure(string name) {
+            return Respond(
+                await _serviceLink.CR_GetGameForAdventure(name, CreateCredentials())
+            );
+        }
+
+        [Authorize]
+        [Route("start/{name}")]
+        public async Task<IActionResult> StartGame(string name) {
+            return Respond(
+                await _serviceLink.CR_StartGame(name, CreateCredentials())
+            );
+        }
     }
 }
